f8f8de079055d21f00a3c1795dfb27f6dd08d025
Francis Tan <francis.adrian.tan@gmail.com>
2016-03-12 16:49:32 -0800

    Install script runs without errors on OS X

--- install.sh
+++ install.sh
@@ -8,29 +8,30 @@ sudo echo "Installing dotfiles"
 
 # Check to see if we are running on OS X
 if [ "$(uname)" == "Darwin" ]; then
-	echo "Running on OS X"
-	
-	# Check to see if Homebrew is installed. Install otherwise
-	if test ! $(which brew); then
-		echo "Installing Homebrew"
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"	
-	fi
+    echo "Running on OS X"
+    # Check to see if Homebrew is installed. Install otherwise
+    if test ! $(which brew); then
+        echo "Installing Homebrew"
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"	
+    fi
 
-	echo "Installing packages"
-	brew install wget
-	brew install zsh
-	brew install tree
-	brew install git-flow
+    echo "Installing packages"
+    brew install wget
+    brew install zsh
+    brew install tree
+    brew install git-flow
+    brew install vim
 else
-	# sudo apt-get -y install zsh emacs24 tree multitail git-flow
-	# sudo apt-get -y install $PACKAGES
-	# rm ~/.zshrc
-	# ln -s ~/.environment_settings/.emacs ~/.emacs
-	# sudo chsh -s $(which zsh)
-	sudo apt-get -y install wget
-	sudo apt-get -y install zsh
-	sudo apt-get -y install tree
-	sudo apt-get -y install git-flow
+    # sudo apt-get -y install zsh emacs24 tree multitail git-flow
+    # sudo apt-get -y install $PACKAGES
+    # rm ~/.zshrc
+    # ln -s ~/.environment_settings/.emacs ~/.emacs
+    # sudo chsh -s $(which zsh)
+    sudo apt-get -y install wget
+    sudo apt-get -y install zsh
+    sudo apt-get -y install tree
+    sudo apt-get -y install git-flow
+    sudo apt-get -y install vim
 fi
 
 echo "Installing pip for Python"
@@ -38,7 +39,8 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 rm get-pip.py
 sudo pip install virtualenv
-sudo pip install virtualenvwrapper
+sudo pip install --no-deps virtualenvwrapper
+sudo pip install --no-deps stevedore
 
 echo "Installing oh-my-zsh"
 rm ~/.zshrc
@@ -53,6 +55,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/vim/bundle/Vund
 echo "Creating symbolic links"
 rm ~/.zshrc
 rm ~/.emacs
+rm ~/.zshrc.pre-oh-my-zsh
 ln -s ~/dotfiles/zsh/toothed.zsh-theme ~/.oh-my-zsh/themes/toothed.zsh-theme
 ln -s ~/dotfiles/zsh/zshrc.symlink ~/.zshrc
 ln -s ~/dotfiles/emacs/emacs.symlink ~/.emacs
